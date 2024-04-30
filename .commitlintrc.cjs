a9144e25bf5bcfefc6094634084612b4b8d9dc49
panjiajie <panjiajie@chexiao.co>
2024-04-30 16:26:17 +0800

    build: 修改cz的规则

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -10,6 +10,31 @@ module.exports = {
     types: [
       { value: "feat", name: "feat:新增功能 | A new feature" },
       { value: "fix", name: "fix:修复bug | fix bugs" },
+      { value: "style", name: "style:修改样式 | change styles" },
+      { value: "docs", name: "docs: 文档更新 | Documentation only changes" },
+      {
+        value: "refactor",
+        name: "refactor: 代码重构 | neither fixes a bug nor adds a feature",
+      },
+
+      { value: "perf", name: "perf: 性能提升 | improve performance" },
+      {
+        value: "test",
+        name: "test: 测试相关 | Adding missing tests or correcting existing tests",
+      },
+      {
+        value: "build",
+        name: "build:构建相关 | build system or external dependencies",
+      },
+      {
+        value: "ci",
+        name: "ci: 持续集成 | Changes to our CI configuration files and scripts",
+      },
+      { value: "revert", name: "revert: 回退代码 | Revert to a commit" },
+      {
+        value: "chore",
+        name: "chore:其他修改 | Other changes that do not modify src or test files",
+      },
     ],
   },
 };
