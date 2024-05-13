3d0d3e6f1648e8844f28396da605faea9e111588
panjiajie <panjiajie@chexiao.co>
2024-05-13 14:43:07 +0800

    feat: mission complete

---
+++ src/types/type.ts
@@ -0,0 +1,6 @@
+export interface Opt {
+  row?: number; // 行数
+  col?: number; // 列数
+  color?: string; // 字体颜色
+  fontSize?: string; // 字体大小
+}
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,10 +1,18 @@
+import { Opt } from "../types/type";
+
 // 测试typescript的功能 和 技术调研
 const classNamSpace = "util";
 
-const commonStyle: string =
-  "position: absolute;transform: rotate(-30deg);pointer-events: none";
-
-const createWaterMark = (str: string = "", elementId: string = "") => {
+const createWaterMark = (
+  str: string = "",
+  elementId: string = "",
+  opt: Opt = {
+    row: 4,
+    col: 4,
+    color: "rgba(239,239,239, 0.74)",
+    fontSize: "15px",
+  }
+) => {
   if (!str) {
     return;
   }
@@ -16,27 +24,39 @@ const createWaterMark = (str: string = "", elementId: string = "") => {
   if (!el) {
     return;
   }
-
-  const spans: Element[] = createSpan(str);
+  const counts = opt.col && opt.row ? opt.col * opt.row : 12;
+  const spans: Element[] = createSpan(str, counts);
   const container: Element = document.createElement("div");
   container.setAttribute("class", `${classNamSpace}_container`);
+  container.setAttribute("style", adaptStyle(opt));
   for (let i = 0; i < spans.length; i++) {
     container.appendChild(spans[i]);
   }
   el.appendChild(container);
 };
 
-const createSpan = (text: string = ""): Element[] => {
+const createSpan = (text: string = "", counts: number = 12): Element[] => {
   const spanArray: Element[] = [];
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < counts; i++) {
     const span: Element = document.createElement("span");
-    const style: string = `${commonStyle};left:${i * 200}px;top:${30 * i}px;`;
-    span.setAttribute("style", style);
     span.setAttribute("class", `${classNamSpace}_water_mark`);
     span.appendChild(document.createTextNode(text));
     spanArray.push(span);
   }
   return spanArray;
+};
+
+/**
+ * 调整样式
+ */
+const adaptStyle = (opt: Opt): string => {
+  const col = opt.col || 4;
+  const row = opt.row || 4;
+  const fontSize = opt.fontSize || "15px";
+  return `color:${opt.color};
+	font-size:${fontSize};
+	grid-template-columns:repeat(${col}, ${100 / col}%);
+	grid-template-rows: repeat(${row}, ${100 / row}%);`;
 };
 
 export default {
