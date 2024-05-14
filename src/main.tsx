4d42432f454d413573059b6bfb59f38139f84fbd
panjiajie <panjiajie@chexiao.co>
2024-05-14 12:06:19 +0800

    perf

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import  "./assets/util.scss"
 import WaterMark from "./utils/utils.ts"
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+  
   </React.StrictMode>
 )
 
