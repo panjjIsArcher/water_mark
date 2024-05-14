cfccd98b985bf95b0a0d5ef8307cd82d5abc6caf
panjiajie <panjiajie@chexiao.co>
2024-05-14 18:14:32 +0800

    readme.md

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,7 @@ import  "./assets/util.scss"
 import WaterMark from "./utils/utils.ts"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-  
-  </React.StrictMode>
+  <React.StrictMode/> 
 )
 
 export {
